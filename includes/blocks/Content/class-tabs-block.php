46570fef40465ede74e831a637e8091877bc3ba9
Abhijit Bhatnagar <abhijit.bhatnagar@gmail.com>
2025-09-26 12:11:49 +0530

    fixed classname issue and PHP fatal error

--- includes/blocks/Content/class-tabs-block.php
+++ includes/blocks/Content/class-tabs-block.php
@@ -14,6 +14,13 @@ namespace Forjeon\Blocks\Content;
 class Tabs_Block {
 
 	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		$this->init();
+	}
+
+	/**
 	 * Initialize the tabs block
 	 */
 	public function init() {
@@ -99,11 +106,70 @@ class Tabs_Block {
 	/**
 	 * Render the tabs block
 	 *
+	 * @param array    $attributes Block attributes.
+	 * @param string   $content    Block content.
+	 * @param WP_Block $block      Block instance.
 	 * @return string Rendered block HTML.
 	 */
-	public function render_block() {
-		// Include the render template.
-		return include FORJEON_PLUGIN_DIR . 'templates/blocks/tabs/render.php';
+	public function render_block( $attributes, $content, $block ) {
+		return $this->render_tabs_html( $attributes, $content, $block );
+	}
+
+	/**
+	 * Render tabs HTML
+	 *
+	 * @param array    $attributes Block attributes.
+	 * @param string   $content    Block content.
+	 * @param WP_Block $block      Block instance.
+	 * @return string Rendered block HTML.
+	 */
+	public function render_tabs_html( $attributes, $content, $block ) {
+		// Extract attributes.
+		$tabs = $attributes['tabs'] ?? array();
+		$active_tab = $attributes['activeTab'] ?? 0;
+		$tab_style = $attributes['tabStyle'] ?? 'default';
+		$tab_alignment = $attributes['tabAlignment'] ?? 'left';
+		$enable_accordion = $attributes['enableAccordionOnMobile'] ?? true;
+		$mobile_breakpoint = $attributes['mobileBreakpoint'] ?? 768;
+
+		// Early return for empty tabs.
+		if ( empty( $tabs ) ) {
+			return '<div class="forjeon-tabs-debug">No tabs data found. Attributes: ' . wp_json_encode( $attributes ) . '</div>';
+		}
+
+		// Generate unique ID and classes.
+		$tabs_id = 'forjeon-tabs-' . wp_unique_id();
+		$classes = array(
+			'forjeon-tabs-container',
+			'forjeon-tabs-' . $tab_style,
+			'forjeon-tabs-align-' . $tab_alignment,
+		);
+
+		$wrapper_attributes = get_block_wrapper_attributes(
+			array(
+				'class' => implode( ' ', $classes ),
+				'id' => $tabs_id,
+				'data-active-tab' => $active_tab,
+				'data-tab-style' => $tab_style,
+				'data-tab-alignment' => $tab_alignment,
+				'data-enable-accordion' => $enable_accordion ? 'true' : 'false',
+				'data-mobile-breakpoint' => $mobile_breakpoint,
+			)
+		);
+
+		// Build HTML using static methods.
+		$html = sprintf( '<div %s>', $wrapper_attributes );
+		$html .= self::render_tabs_navigation( $tabs, $active_tab );
+		$html .= '<div class="forjeon-tabs-content">';
+
+		foreach ( $tabs as $index => $tab ) {
+			$is_active = $index === $active_tab;
+			$html .= self::render_tab_panel( $tab, $index, $is_active, $enable_accordion );
+		}
+
+		$html .= '</div></div>';
+
+		return $html;
 	}
 
 	/**
